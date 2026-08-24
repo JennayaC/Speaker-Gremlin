f323a3c34554c18f882cb4b709ecbaf7df3b0aa3
Jennaya Horne <jennayajch@gmail.com>
2026-08-24 13:44:58 -0400

    Implemented a set to recover and catch lost nodes if the coordinator loses a heartbeat.

--- src/coordinator.py
+++ src/coordinator.py
@@ -7,8 +7,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('127.0.0.1',5001))
 
 print(f"Listening for heartbeats from nodes...")
+
 detected_nodes = {}
-sock.settimeout(2)
+lost_nodes = set()
+sock.settimeout(1)
 
 while True:
     try:
@@ -16,6 +18,11 @@ while True:
         recv_time = time.time()
 
         msg = json.loads(data.decode())
+        
+        if msg["node"] in lost_nodes:
+            print(f"Node {msg['node']} is back online!")
+            lost_nodes.remove(msg["node"])
+
         detected_nodes[msg["node"]] = recv_time
         print(detected_nodes)
     
@@ -24,8 +31,9 @@ while True:
 
     for node_id in detected_nodes:
         time_since_last_heartbeat = time.time() - detected_nodes[node_id]
-        if time_since_last_heartbeat > 2.5:
+        if time_since_last_heartbeat > 2.5 and node_id not in lost_nodes:
             print(f"Node {node_id} heartbeat lost")
+            lost_nodes.add(node_id)
         
         
 
